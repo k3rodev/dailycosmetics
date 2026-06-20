b5fc49b96118fac6b02c49b4203aaa0ac89e7e92
k3ro <therealfwilson71@gmail.com>
2026-06-20 01:48:59 -0500

    Create index.js

---
+++ index.js
@@ -0,0 +1,18 @@
+var cosmetics = 
+[
+    { id: "Santa Set", weight: 5 },
+    { id: "broom", weight: 15 },
+    { id: "Turkey Leg", weight: 15 },
+    { id: "Snake Eyes", weight: 30 },
+    { id: "Wrapped lights", weight: 10 },
+    { id: "Angel DLC", weight: 5 },
+    { id: "Blossom Hat", weight: 35 },
+    { id: "Fried Egg", weight: 60 },
+    { id: "teeth", weight: 50 },
+    { id: "bone", weight: 25 },
+    { id: "Nose Butter Fly", weight: 25 },
+    { id: "Aura Badge", weight: 1 },
+    { id: "Rosey Cheeks", weight: 15 },
+    { id: "Leaf Mask", weight: 20 },
+    { id: "Flower body wrap", weight: 15 }
+];
